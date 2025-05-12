384d346d7498f653349792ca7db40ed35709f7a9
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-05-12 16:13:20 +0200

    postman_success

--- app.js
+++ app.js
@@ -2,10 +2,47 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+express.static("public");
+
 app.get("/", (req, res) => {
-  console.log(`ricevuta richiesta GET sulla rotta "/"`);
+  console.log(`Server del mio blog`);
+});
+
+app.get("/bacheca", (req, res) => {
+  res.type("json").send([
+    {
+      titolo: "Ciambellone",
+      contenuto: "ciambellone cioccolato",
+      immagine: "/ciambellone.jpeg",
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      titolo: "craker",
+      contenuto: "cracker barbabietola",
+      immagine: "/craker_barbabietola.jpeg",
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      titolo: "pane fritto",
+      contenuto: "pane fritto dolce",
+      immagine: "/pane_fritto_dolce.jpeg",
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      titolo: "pasta",
+      contenuto: "pasta con barbabietola",
+      immagine: "/pasta_barbabietola.jpeg",
+      tags: ["tag1", "tag2", "tag3"],
+    },
+    {
+      titolo: "torta",
+      contenuto: "torta paesana",
+      immagine: "/torta_paesana.jpeg",
+      tags: ["tag1", "tag2", "tag3"],
+    },
+  ]);
 });
 
 app.listen(port, () => {
-  console.log("il server è in ascolto sulla porta " + port);
+  console.log("Server in ascolto su port " + port);
 });
